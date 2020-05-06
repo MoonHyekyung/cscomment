407210da3782727b1a442ba2f00d3dda53367fbe
MoonHyekyung <moonsoft02@gmail.com>
2020-05-06 23:46:29 +0900

    x=1, y=4인 경우

--- cscomment/Program.cs
+++ cscomment/Program.cs
@@ -11,8 +11,8 @@ namespace cscomment
         static void Main(string[] args)
         {
             int x, y;
-            x = 2;
-            y = 10;
+            x = 1;
+            y = 4;
             if (x > 4)
             {
                 if (y > 2)
